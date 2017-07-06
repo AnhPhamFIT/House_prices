0eab44097ee5db48d25e2d26b40a0b8a381fd325
AnhPhamFIT <ptanh.fit@gmail.com>
2017-07-06 13:38:02 +0700

    First commit

---
+++ Library/cmp.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import sys
+train = pd.read_csv(sys.argv[1])
+test = pd.read_csv(sys.argv[2])
+train["SalePrice"]=train["SalePrice"]-test["SalePrice"]
+def neg(x):
+	if x<0:
+		x=-1*x
+	return x
+train["SalePrice"]=train["SalePrice"].apply(neg)
+train.to_csv("sol3.csv", index = False)
+print (train["SalePrice"].sum())
---
+++ Library/header_files.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as pl
+from sklearn.linear_model import Lasso
+import warnings
+import seaborn as sns
+from scipy.stats import skew
+from scipy.stats.stats import pearsonr
+from sklearn.cross_validation import cross_val_score
+from sklearn.metrics import make_scorer, mean_squared_error
+from sklearn.linear_model import Ridge, RidgeCV, ElasticNet, LassoCV, LassoLarsCV
+from sklearn.model_selection import cross_val_score
+from operator import itemgetter
+import itertools
+import xgboost as xgb
+from sklearn.svm import SVC
+from itertools import product, chain
